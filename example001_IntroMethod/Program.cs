046b8a0cfd74647bf23eb6869baf22a30127e323
CJINBNH <alex_egorov_89@mail.ru>
2022-08-07 22:59:46 +0400

    Completed task with function Max

--- example001_IntroMethod/Program.cs
+++ example001_IntroMethod/Program.cs
@@ -1,15 +1,32 @@
 ﻿Console.Clear();
-//стихийный подход, поиск максимального из девяти чисел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
 int a1 = 15;
 int b1 = 21;
 int c1 = 39;
-int a2 = 112;
+int a2 = 11;
 int b2 = 23;
-int c2 = 33;
+int c2 = 353;
 int a3 = 13;
 int b3 = 23;
 int c3 = 33;
-int max = a1;
+
+int max1 = Max (a1, b1, c1);
+int max2 = Max (a2, b2, c2);
+int max3 = Max (a3, b3, c3);
+int max = Max (max1, max2, max3);
+
+Console.WriteLine(max);
+
+//стихийный подход, поиск максимального из девяти чисел
+//int max = a1;
 // if (b1 > max) max = b1;
 // if (c1 > max) max = c1;
 
@@ -20,5 +37,3 @@ int max = a1;
 // if (a3 > max) max = a3;
 // if (b3 > max) max = b3;
 // if (c3 > max) max = c3;
-
-// Console.WriteLine(max);
