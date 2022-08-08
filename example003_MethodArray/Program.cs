0803b6c2e2baa732ba77b3f5db3c05031b8160a3
CJINBNH <alex_egorov_89@mail.ru>
2022-08-08 13:37:10 +0400

    Comleted example003

---
+++ example003_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+int[] array = { 9, 8, 7, 2, 5, 4, 3, 2 };
+int n = array.Length;
+int find = 2;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.Write("Искомый элемент массива имеет index = ");
+        Console.WriteLine(index);
+        break;
+    }
+        index++;
+}
